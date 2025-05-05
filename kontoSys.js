96860405320148ac79b91c20d476300353b0a5ec
SebastianFraKontraktika <sebmlin0818@gmail.com>
2025-05-05 09:52:27 +0200

    prototyp innlogg system

---
+++ kontoSys.js
@@ -0,0 +1,24 @@
+
+function submit() {
+    let brukNavn = document.getElementById("bNavn").value;
+    let passord = document.getElementById("pOrd").value;
+    localStorage.setItem(brukNavn, passord)
+}
+
+function logg() {
+    for (var i = 0; i < localStorage.length; i++){
+        let loggBruker = document.getElementById("loggBruk").value;
+        let loggPass = document.getElementById("loggPass").value;
+        let bruker = localStorage.key(i)
+        console.log(bruker)
+        console.log(localStorage.getItem(bruker))
+        if (loggBruker == bruker && loggPass == localStorage.getItem(bruker)) {
+            console.log("hurra")
+            break
+        } else {
+            console.log("fitte")
+        }
+
+    }
+
+}
