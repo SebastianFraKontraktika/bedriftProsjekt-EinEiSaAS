a9ecb00e95d48fbb8e9b5cb3b875037e0f5da6dc
SebastianFraKontraktika <sebmlin0818@gmail.com>
2025-05-12 11:02:55 +0200

    cookies

--- src/kontoSys.js
+++ src/kontoSys.js
@@ -1,3 +1,14 @@
+x = document.cookie
+console.log(x)
+
+if(x == "value=true") {
+    console.log("hallo")
+    let K = document.getElementById("kontoKp");
+    let I = document.getElementById("innlogging");
+    K.style.display = 'block';
+    I.style.display = 'none';
+}
+
 function submit() {
     let brukNavn = document.getElementById("bNavn").value;
     let passord = document.getElementById("pOrd").value;
@@ -17,6 +28,7 @@ function logg() {
             let innloggKp = document.getElementById("innlogging")
             kontoKp.style.display = 'block'
             innloggKp.style.display = 'none'
+            document.cookie = "value=true"
             break
         } else {
             console.log("feil")
@@ -46,4 +58,8 @@ function visBruk() {
     } else {
         B.style.display = 'none';
     }
+}
+function reset() {
+    document.cookie = "value=false"
+    localStorage.clear()
 }
